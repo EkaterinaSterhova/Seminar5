2e3c2eaf36351c3776ab840845e7d0deaae2082b
EkaterinaSterhova <126870774+EkaterinaSterhova@users.noreply.github.com>
2023-03-25 14:07:53 +0600

    36

---
+++ Задача 34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34
+// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+ void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000); // [100, 999]
+}
+
+int ChetElements(int[] array)
+{
+    int count=0;
+    for (int i = 0; i < array.Length / 2; i++)
+    { 
+        count++;  
+    }
+    return count;
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ChetElements(array);
+Console.WriteLine(ChetElements(array));
+
---
+++ Задача 36/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+ void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-20, 21);//[-20.20] 
+}
+
+int NEChetElements(int[] array)
+{
+    int sum=0;
+    for (int i=0; i< array.Length; i++)
+    { 
+    if(i%2!=0)
+       {
+         sum+=array[i];
+        }  
+    }
+    return sum;
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+NEChetElements(array);
+Console.WriteLine(NEChetElements(array));
+
+
+
